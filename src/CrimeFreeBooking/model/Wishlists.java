40e01dfcce143ca9a4bd04f8e0f8cad4f3377d89
Donald Hall <hall.do@husky.neu.edu>
2018-11-10 11:18:15 -0800

    model update

---
+++ src/CrimeFreeBooking/model/Wishlists.java
@@ -0,0 +1,46 @@
+package CrimeFreeBooking.model;
+
+public class Wishlists {
+	protected int wishlistId;
+	protected Users user;
+	protected Listings listing;
+	
+	public Wishlists(int wishlistId, Users user, Listings listing) {
+		this.wishlistId = wishlistId;
+		this.user = user;
+		this.listing = listing;
+	}
+
+	public Wishlists(int wishlistId) {
+		this.wishlistId = wishlistId;
+	}
+
+	public Wishlists(Users user, Listings listing) {
+		this.user = user;
+		this.listing = listing;
+	}
+
+	public int getWishlistId() {
+		return wishlistId;
+	}
+
+	public void setWishlistId(int wishlistId) {
+		this.wishlistId = wishlistId;
+	}
+
+	public Users getUser() {
+		return user;
+	}
+
+	public void setUser(Users user) {
+		this.user = user;
+	}
+
+	public Listings getListing() {
+		return listing;
+	}
+
+	public void setListing(Listings listing) {
+		this.listing = listing;
+	}
+}
